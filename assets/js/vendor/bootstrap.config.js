84b9a9d5d10a9f2e8980149e5bd0b1bb7b982072
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-08-03 14:03:02 +0200

    fixed missing empty line, quotes

--- assets/js/vendor/bootstrap.config.js
+++ assets/js/vendor/bootstrap.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	styleLoader: require('extract-text-webpack-plugin').extract('style-loader', 'css-loader!postcss-loader!less-loader'),
 	scripts: {
-		'dropdown': true
+		"dropdown": true
 	},
 	styles: {
 		"mixins": true,
