9e6e05e500c243429c095757a1bc18d208eb3860
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-08-04 12:43:16 +0200

    removed additional space

--- theme/functions.php
+++ theme/functions.php
@@ -91,7 +91,7 @@ if ( ! function_exists( 'usine_setup' ) ) {
 
 		// Let wp know we want to use post thumbnails
 		add_theme_support( 'post-thumbnails', array( 'page', 'vox' ) );
-		
+
 		// Add Custom Logo Support.
 		/*
 		add_theme_support( 'custom-logo', array(
