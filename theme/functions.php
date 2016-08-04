0720d3bcf18de93d38c0ebd79c42b62f23027020
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-08-04 12:18:57 +0200

    added missing tgmpa include function

--- theme/functions.php
+++ theme/functions.php
@@ -47,6 +47,8 @@ if ( class_exists( 'WPLessPlugin' ) ) {
 
 add_action( 'after_setup_theme',  'usine_setup' );
 
+add_action( 'tgmpa_register', 'usine_register_required_plugins' );
+
 add_action( 'wp_enqueue_scripts', 'usine_styles' );
 
 add_action( 'wp_enqueue_scripts', 'usine_scripts' );
@@ -123,6 +125,33 @@ if ( ! function_exists( 'usine_setup' ) ) {
 	}
 }
 
+if ( ! function_exists( 'usine_register_required_plugins' ) ) {
+	function usine_register_required_plugins() {
+		$plugins = array(
+			// This is an example of how to include a plugin from the WordPress Plugin Repository.
+			array(
+				'name'      => 'WP-LESS',
+				'slug'      => 'wp-less',
+				'required'  => true,
+			),
+		);
+
+		$config = array(
+			'id'           => 'usine.ch',              // Unique ID for hashing notices for multiple instances of TGMPA.
+			'default_path' => '',                      // Default absolute path to bundled plugins.
+			'menu'         => 'tgmpa-install-plugins', // Menu slug.
+			'parent_slug'  => 'themes.php',            // Parent menu slug.
+			'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the 	parent menu used.
+			'has_notices'  => true,                    // Show admin notices or not.
+			'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+			'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+			'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+			'message'      => '',                      // Message to output right before the plugins table.
+		);
+
+		tgmpa( $plugins, $config );
+	}
+}
 
 /**
  * Register and/or Enqueue
