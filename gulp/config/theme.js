a91fc42bb5fd4d92416f8b2a4e2b6377f06f9eba
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-08-04 12:24:47 +0200

    added missing less file extension to processed files in webpack theme config override

--- gulp/config/theme.js
+++ gulp/config/theme.js
@@ -4,4 +4,18 @@
  * Config
  *
  */
-module.exports = {};
+
+// config
+var paths = require('../core/config/common').paths;
+var theme = paths.theme;
+
+module.exports = {
+	paths: {
+		watch: theme.src + '/**/*.{json,php,png,jpg,less}',
+		src:   theme.src + '/**/*.{json,php,png,jpg,less}',
+		clean: [
+			theme.dest + '/**/*.{css,json,php,png,less}',
+			'!' + paths.assets.dest + '/**/*'
+		]
+	},
+};
