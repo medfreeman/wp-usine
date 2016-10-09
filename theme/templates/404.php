28051ddc87adf27e73bbffbbecfabba782ba59fb
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-10-09 11:44:30 +0200

    fix: 404 translation. fixes #7

--- theme/templates/404.php
+++ theme/templates/404.php
@@ -1,11 +1,11 @@
 				<article id="post-not-found">
 
 					<header>
-						<h1>Not Found</h1>
+						<h1>Non trouvé</h1>
 					</header>
 
 					<section class="post_content">
-						<p>Sorry, but the requested resource was not found on this site.</p>
+						<p>Désolé, mais le contenu demandé n'a pas été trouvé sur le site.</p>
 					</section>
 
 				</article>
