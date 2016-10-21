b5f40830af3aa2c15be4c4f0c76fbef45ca8e8b3
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-10-21 03:21:32 +0200

    change: support for no heading page in bla and vox archive pages

--- theme/elements/content-bla.php
+++ theme/elements/content-bla.php
@@ -7,18 +7,20 @@
 
 ?>
 <?php
-	global $post;
-	$args = array(
-		'page_id' => absint( get_theme_mod( USINE_THEME_MOD_BLA_PAGE ) ),
-	);
-	$page_query = new WP_Query( $args );
+$page_id = absint( get_theme_mod( USINE_THEME_MOD_BLA_PAGE ) );
+if ( ! $page_id ) {
+	return;
+}
+$args = array(
+	'post_type' => 'page',
+	'page_id'   => $page_id,
+);
+$page_query = new WP_Query( $args );
 
-	if ( $page_query->have_posts() ) :
-		$page_query->the_post();
-		$post_data = $page_query->post;
-		setup_postdata( $post_data );
-
-		the_content();
-
-		wp_reset_postdata();
-	endif;
+if ( $page_query->have_posts() ) :
+	$page_query->the_post();
+	$post_data = $page_query->post;
+	setup_postdata( $post_data );
+	the_content();
+	wp_reset_postdata();
+endif;
